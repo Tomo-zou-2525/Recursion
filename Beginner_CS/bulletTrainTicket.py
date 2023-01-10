44332343185eaedbb2db1da598aa411fcad9cc12
友造-2525 <bathbook1990tomozou@gmail.com>
2023-01-10 23:45:24 +0900

    answered

---
+++ Beginner_CS/bulletTrainTicket.py
@@ -0,0 +1,13 @@
+def weekly7DaysSales(ticketPrice):
+    total = 150000
+    changeTotalSum = 700
+    price = 250
+    # print(ticketPrice)
+    # print(price - ticketPrice)
+    # print(changeTotalSum * (price - ticketPrice))
+    # s = changeTotalSum * (price - ticketPrice)
+    # print(total + s)
+    print(total + changeTotalSum * (price - ticketPrice))
+
+
+weekly7DaysSales(220)
