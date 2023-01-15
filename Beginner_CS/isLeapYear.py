391212361fc8ffd4b63b90ad8ae319047b78f554
友造-2525 <bathbook1990tomozou@gmail.com>
2023-01-15 22:36:05 +0900

    isLeapYear Done

--- Beginner_CS/isLeapYear.py
+++ Beginner_CS/isLeapYear.py
@@ -1,7 +1,7 @@
 def isLeapYear(year):
     # 関数を完成させてください
     year = int(year)
-    if year % 4 == 0 and year % 100 != 0 and year % 400 == 0:
+    if year % 400 == 0 or year % 100 != 0 and year % 4 == 0:
         return True
     else:
         return False
