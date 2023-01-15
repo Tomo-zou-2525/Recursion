1d19a0c4484456e948c11fab2d66f7ac613e78d5
友造-2525 <bathbook1990tomozou@gmail.com>
2023-01-15 18:06:38 +0900

    isVowl fix

--- Beginner_CS/isVowl.py
+++ Beginner_CS/isVowl.py
@@ -1,14 +1,11 @@
 # 例題
 # 小文字が与えられるので、母音なら true、子音なら false を返す、isVowel という関数を作成してください。
 def isVowel(word):
-    if word == "a" or "i" or "u" or "e" or "o":
-        print(word)
-        return print("true")
-    else:
-        print("false")
+    return word == "a" or word == "i" or word == "u" or word == "e" or word == "o"
 
 
 # isVowel('a')  # -> true
 # isVowel('e') -> true
 # isVowel('z') -> false
-isVowel('k')  # -> false
+print(isVowel('a'))
+print(isVowel('k'))  # -> false
