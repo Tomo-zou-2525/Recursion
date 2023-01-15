83d804c957ed81ef40c1635a3dcbe73130aa9f51
友造-2525 <bathbook1990tomozou@gmail.com>
2023-01-15 22:15:08 +0900

    myXOR Done

---
+++ Beginner_CS/xor.py
@@ -0,0 +1,17 @@
+from operator import xor
+
+
+def myXOR(p, q):
+    # 関数を完成させてください
+    if p != q:
+        return True
+    else:
+        return False
+
+
+print(xor(True, False))
+
+# myXOR(True, True)
+# myXOR(True, False)
+# myXOR(False, True)
+# myXOR(False, False)
