ef496ef354cafc64236242f8048d48cb6507e518
友造-2525 <bathbook1990tomozou@gmail.com>
2023-01-15 17:53:53 +0900

    echo Done

---
+++ Beginner_CS/echo.py
@@ -0,0 +1,15 @@
+def echo(val):
+    if val % 15 == 0:
+        return print("fizzbuzz")
+    elif val % 5 == 0:
+        return print("buzz")
+    elif val % 3 == 0:
+        return print("fizz")
+    else:
+        print("NOT FOUND!")
+
+
+echo(3)  # -> fizz
+echo(5)  # -> buzz
+echo(15)  # -> fizzbuzz
+echo(67)  # -> NOT FOUND!
