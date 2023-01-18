de02f9c501892f7ebdb3a5fb0d686899f1d1a1f6
友造-2525 <bathbook1990tomozou@gmail.com>
2023-01-19 00:24:41 +0900

    recursion Done

--- Beginner_CS/isThereSchool.py
+++ Beginner_CS/isThereSchool.py
@@ -1,6 +1,16 @@
-def isThereSchool(day,isHoliday):
+from unittest import removeResult
+
+
+def isThereSchool(day, isHoliday):
     # 関数を完成させてください
-    
+    if day == "Sunday" or day == "Saturday" or isHoliday == True:
+        return False
+    else:
+        return True
 
 
-
+print(isThereSchool("Sunday", True))
+print(isThereSchool("Monday", True))
+print(isThereSchool("Monday", False))
+print(isThereSchool("Unko", False))
+print(isThereSchool("Unko", True))
