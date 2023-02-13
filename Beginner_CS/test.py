9ae7f19a32f7960979bf69d147f1e2165cc4129e
友造-2525 <bathbook1990tomozou@gmail.com>
2023-02-14 07:41:14 +0900

    python use network test example

--- Beginner_CS/test.py
+++ Beginner_CS/test.py
@@ -10,12 +10,43 @@
 
 # print(circleArea(r))
 
-r = 100
-otherSolution = 3.14
+# r = 100
+# otherSolution = 3.14
 
 
-def circleArea(radius):
-    return r * r
+# def circleArea(radius):
+#     return r * r
 
 
-print(circleArea(r))
+# print(circleArea(r))
+
+# -*- coding: utf-8 -*-
+import subprocess
+
+
+class Ping(object):
+    def __init__(self, hosts):
+        for host in hosts:
+            # pingコマンド
+            ping = subprocess.Popen(["ping", "-c", "1", host],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE
+                                    )
+
+            # ping試験
+            out, error = ping.communicate()
+
+            # 接続できなかったら
+            if error:
+                print('[NG]: ' + host + ', Msg->\'' + error.rstrip() + '\'')
+
+            # 接続できたら
+            else:
+                print('[OK]: ' + host)
+
+
+# ping試験するホスト
+hosts = ['www.google.com', 'algorithm.joho.info', ]
+
+# ping試験
+Ping(hosts)
