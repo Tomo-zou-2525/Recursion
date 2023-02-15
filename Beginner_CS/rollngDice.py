03f756e9547f4d15b4546707f72b4b878b0e0b76
友造-2525 <bathbook1990tomozou@gmail.com>
2023-02-16 03:33:23 +0900

    rollingDice Done

--- Beginner_CS/rollngDice.py
+++ Beginner_CS/rollngDice.py
@@ -1,11 +1,23 @@
 from itertools import count
 
 
-def rollingDice(countA, countB):
-    for i in range(countA):
-        judge = 6 * countA
-        if 0 < countB < judge:
-            print("うんこ")
+# def rollingDice(countA, countB):
+#     for i in range(countA):
+#         judge = 6 * countA
+#         if 0 < countB < judge:
+#             print("うんこ")
 
 
-rollingDice(10, 61)
+# rollingDice(10, 61)
+def rollingDice():
+    A, B = map(int, input().split())
+    print(A)
+    print("---")
+    print(B)
+    if B <= A*6 and B >= A:
+        print('Yes')
+    else:
+        print('No')
+
+
+rollingDice()
