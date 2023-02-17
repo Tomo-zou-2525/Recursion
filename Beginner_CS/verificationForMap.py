11d095cb8ad927e4ec86cb6c090f077a7378a399
友造-2525 <bathbook1990tomozou@gmail.com>
2023-02-17 23:59:08 +0900

    iterable

--- Beginner_CS/verificationForMap.py
+++ Beginner_CS/verificationForMap.py
@@ -2,3 +2,10 @@
 # List(配列)の各要素毎に処理をしたい場合に使用する関数の種類
 # 戻り値はmapオブジェクト
 # イテレータの一種（forループで回せるやつ）
+
+a = [100, 200, 300]
+# next(a) <- エラー！
+print(next(a))
+a_iter = iter(a)
+print(next(a_iter))  # 100が取り出される
+print(a_iter)
